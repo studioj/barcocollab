495c89c6a176df0b055828676fc24c797c6b816d
studioj <22102283+studioj@users.noreply.github.com>
2024-04-26 16:34:55 +0200

    initial commit

---
+++ __init__.py
@@ -0,0 +1,9 @@
+"""Empty initialization file for barcocollab package"""
+
+def say_hi():
+  """raise a simple exception message"""
+  raise Exception("Hi from barcocollab package, you've reached the wrong package please use the internal pypi package!")
+
+# Add more functions or classes here as needed
+
+say_hi()
